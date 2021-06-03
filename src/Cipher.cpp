0d4e41f4662b3f0c8ccae123afb09a3a82da5af5
lucinda27 <81180210+lucinda27@users.noreply.github.com>
2021-06-03 00:42:07 -0600

    Update and rename Cipher to Cipher.cpp

---
+++ src/Cipher.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include "Cipher.h"
+using namespace std;
+
+Cipher :: Cipher() {}
+
+void Cipher :: setPlaintext(const string ptext) {}
+
+string Cipher :: noSpace() {
+  string nospace="";
+  return nospace;
+}
+int Cipher :: setLength() {
+  int l=0;
+  return l;
+}
+string Cipher :: lowerCase() {
+  string case="";
+  return case;
+}
+string Cipher :: subString() {
+  string substring="";
+  return substring;
+}
+int Cipher :: detPos() {
+  int pos=0;
+  return pos;
+}
+int Cipher :: movePos() {
+  int pos=0;
+  return pos;
+}
+int Cipher :: revPos() {
+  int pos=0;
+  return pos;
+}
+string Cipher :: enCipher() {
+  string e="";
+  return e;
+}
+string Cipher :: deCipher() {
+  string d="";
+  return d;
+}
