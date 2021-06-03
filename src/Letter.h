83859a1946e3e85c67ebbf65a36315ae8edad37d
lucinda27 <81180210+lucinda27@users.noreply.github.com>
2021-06-03 00:43:53 -0600

    Create Letter.h

---
+++ src/Letter.h
@@ -0,0 +1,15 @@
+#ifndef Letter_H
+#define Letter_H
+#include <string>
+using namespace std;
+
+class Cipher
+{
+      private:
+      string message;
+
+      public:
+      Cipher();
+      void setMessage(const string m);
+};
+#endif
